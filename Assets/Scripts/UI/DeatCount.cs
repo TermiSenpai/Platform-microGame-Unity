570312ee813395c925bcefddea04b01032c5d8c6
Termi <escarpaprieto@gmail.com>
2021-10-13 10:02:11 +0200

    Death Count

---
+++ Assets/Scripts/UI/DeatCount.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DeatCount : MonoBehaviour
+{
+    public TMP_Text textCount;
+
+    private int contador = 0;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void updateDeath(int death)
+    {
+        contador = death;
+        textCount.text = "DEATHS: " + contador;
+    }
+}
